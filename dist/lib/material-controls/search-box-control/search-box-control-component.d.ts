600e281ffda36cc542de6bdad79cefe79621e0cf
Daniel Yoshida <dyoshida@sofrecom.com.ar>
2022-07-12 01:09:20 -0300

    R 1.8.3

--- dist/lib/material-controls/search-box-control/search-box-control-component.d.ts
+++ dist/lib/material-controls/search-box-control/search-box-control-component.d.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from '@angular/core';
+import { ElementRef, EventEmitter } from '@angular/core';
 import { ErrorMaterialComponent } from '../error/error.component';
 import * as i0 from "@angular/core";
 export declare class SearchBoxMaterialControlComponent {
@@ -6,7 +6,9 @@ export declare class SearchBoxMaterialControlComponent {
     disabled: boolean;
     onInput: EventEmitter<string>;
     errorControl: ErrorMaterialComponent;
+    input: ElementRef;
     onInputEvent(val: string): void;
+    reset(emit?: boolean): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<SearchBoxMaterialControlComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<SearchBoxMaterialControlComponent, "sof-search-box-control", never, { "placeholder": "placeholder"; "disabled": "disabled"; }, { "onInput": "onInput"; }, never, never>;
 }
