95c3d998df934558641a03b31bbeef9c87880fb4
iflores <iflores@sofrecom.com.ar>
2022-07-25 15:30:55 -0300

    R 1.12.1

--- dist/lib/material-controls/prefix-control/prefix-control.component.d.ts
+++ dist/lib/material-controls/prefix-control/prefix-control.component.d.ts
@@ -1,6 +1,8 @@
 import { PrefixControlComponent } from '../../base/prefix-control/prefix-control';
+import { ErrorMaterialComponent } from '../error/error.component';
 import * as i0 from "@angular/core";
 export declare class PrefixControlMaterialComponent extends PrefixControlComponent {
+    errorControl: ErrorMaterialComponent;
     inputType: 'number' | 'text';
     static ɵfac: i0.ɵɵFactoryDeclaration<PrefixControlMaterialComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<PrefixControlMaterialComponent, "sof-prefix-control", never, { "inputType": "inputType"; }, {}, never, never>;
