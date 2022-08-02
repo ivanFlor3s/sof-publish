c08a3006433fdc1d42f1d51f2ca9f8de67ef33da
iflores <iflores@sofrecom.com.ar>
2022-08-02 09:36:13 -0300

    R 2.6.4 Custom card

--- dist/lib/i18n-wrapper-controls/i18n-card/i18n-card.component.d.ts
+++ dist/lib/i18n-wrapper-controls/i18n-card/i18n-card.component.d.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { CardCustomOptions } from 'sof-ng-controls';
 import * as i0 from "@angular/core";
 export declare class I18nCardComponent {
     titleKey: string;
@@ -8,7 +9,8 @@ export declare class I18nCardComponent {
     onToggleExpand: EventEmitter<any>;
     isSubCard: boolean;
     disabled: boolean;
+    customOptions?: CardCustomOptions;
     constructor();
     static ɵfac: i0.ɵɵFactoryDeclaration<I18nCardComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<I18nCardComponent, "app-i18n-card", never, { "titleKey": "titleKey"; "subTitleKey": "subTitleKey"; "expandable": "expandable"; "expanded": "expanded"; "isSubCard": "isSubCard"; "disabled": "disabled"; }, { "onToggleExpand": "onToggleExpand"; }, never, ["[header]", "[body]", "[footer]"]>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<I18nCardComponent, "app-i18n-card", never, { "titleKey": "titleKey"; "subTitleKey": "subTitleKey"; "expandable": "expandable"; "expanded": "expanded"; "isSubCard": "isSubCard"; "disabled": "disabled"; "customOptions": "customOptions"; }, { "onToggleExpand": "onToggleExpand"; }, never, ["[header]", "[body]", "[footer]"]>;
 }
