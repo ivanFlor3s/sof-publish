9d494f5bb168538497b3f1eb240169e41a1807d6
Daniel Yoshida <dyoshida@sofrecom.com.ar>
2022-08-16 17:27:23 -0300

    R 2.6.9

--- dist/lib/i18n-wrapper-controls/i18n-autocomplete-control/i18n-autocomplete-control.component.d.ts
+++ dist/lib/i18n-wrapper-controls/i18n-autocomplete-control/i18n-autocomplete-control.component.d.ts
@@ -3,8 +3,20 @@ import { AutocompleteMaterialControlComponent } from 'sof-ng-controls';
 import { I18nOptions } from '../../shared/models/options';
 import * as i0 from "@angular/core";
 export declare class I18nAutocompleteComponent extends I18nOptions implements AfterViewInit {
+    /**
+     * Delay para filtrar
+     **/
+    debounce: number;
+    /**
+     * Busqueda usado una funcion definida en el componente
+     **/
+    filterFn: (filterText: string) => any[];
+    /**
+     * Busqueda usando un endpoint
+     **/
+    filterEndPoint: string;
     control: AutocompleteMaterialControlComponent;
     ngAfterViewInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<I18nAutocompleteComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<I18nAutocompleteComponent, "app-i18n-autocomplete-control", never, {}, {}, never, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<I18nAutocompleteComponent, "app-i18n-autocomplete-control", never, { "debounce": "debounce"; "filterFn": "filterFn"; "filterEndPoint": "filterEndPoint"; }, {}, never, never>;
 }
